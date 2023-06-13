a4bb916e5e78f1e6acabf87be42a310cdb099f52
Will <wdputnam42@gmail.com>
2023-06-13 14:14:37 -0400

    added barebones typedev and resolver schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+const { User } = require('../models');
+
+const resolvers = {
+    Query: {
+        viewUsers: async () => {
+            return await User.find();
+        }
+    },
+
+    Mutation: {
+        addUser: async (_, args) => {
+            const user = await User.create(args);
+
+            return user;
+        }
+    }
+};
+
+module.exports = resolvers;
