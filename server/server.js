b931fea4415ee8c1a4832b500acbf9860fb4265d
kmoely <kylemoely@gmail.com>
2023-06-13 11:14:21 -0500

    Added apolloServer to server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const path = require("path");
 
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-    console.log("I am here");
-    app.listen(PORT, () => {
-        console.log(`Server is running on PORT ${PORT}!`);
+const startApolloServer = async () => {
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+    db.once('open', () => {
+        console.log("I am here");
+        app.listen(PORT, () => {
+            console.log(`Server is running on PORT ${PORT}!`);
+        });
     });
-});
+}
+
+startApolloServer();
