f86f6fc39d32a4b10ca64616107c3b6d5f2cb66f
kylemoely <kylemoely@gmail.com>
2023-06-14 10:18:12 -0600

    Added pages and component files

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,33 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="text-3xl font-bold underline bg-amber-600">
-        Is this thing on?
-      </div>
+      <Router>
+        <Header />
+        <div className = "container">
+          <Routes>
+            <Route
+            path="/"
+            element={<Home />} />
+            <Route
+            path="/login"
+            element={<Login />} />
+            <Route
+            path="/signup"
+            element={<Signup />} />
+            <Route 
+            path="/pranks"
+            element={<Pranks />} />
+            <Route 
+            path="/orders"
+            element={<Orders />} />
+          </Routes>
+        </div>
+      </Router>
     </ApolloProvider>
 
   );
 }
-
+<div className="text-3xl font-bold underline bg-amber-600">
+        Is this thing on?
+      </div>
 export default App;
