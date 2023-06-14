9d9ac77bbde4f3b4f388264bad793046503c5b92
kylemoely <kylemoely@gmail.com>
2023-06-14 09:48:50 -0600

    Added production code

--- server/server.js
+++ server/server.js
@@ -18,6 +18,11 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if(process.env.NODE_ENV === 'production') {
+    // To READ the react content when it is deployed in the internet
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
