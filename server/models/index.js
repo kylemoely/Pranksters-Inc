9237f0047478b5884265cde8ded5c741dfd515f6
kylemoely <kylemoely@gmail.com>
2023-06-14 09:17:04 -0600

    Update index.js
    changed one letter

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
+const User = require('./user');
 const Order = require('./Order');
 const Prank = require('./Prank')
 // Import other models here if you have additional ones
