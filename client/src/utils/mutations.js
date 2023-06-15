0aa4d87a3155dfee86bda321f808a6bd14d0cbf8
kylemoely <kylemoely@gmail.com>
2023-06-15 11:42:34 -0600

    Added and modified queries and mutations on the back end. Added them to the front end as well

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!) {
+    mutation Mutation($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
         _id
         email
@@ -15,7 +15,7 @@ export const ADD_USER = gql`
 `;
 
 export const ADD_PRANK = gql`
-    mutation addPrank($title: String!, $price: Int!, $description: String!, $inPerson: Boolean!) {
+    mutation Mutation($title: String!, $price: Int!, $description: String!, $inPerson: Boolean!) {
         addPrank(title: $title, price: $price, description: $description, inPerson: $inPerson) {
         _id
         description
@@ -27,7 +27,7 @@ export const ADD_PRANK = gql`
 `;
 
 export const ADD_ORDER = gql`
-    mutation addOrder($location: String!, $prankee: String!, $prank: ID!, $dateTime: String!, $user: ID!) {
+    mutation Mutation($location: String!, $prankee: String!, $prank: ID!, $dateTime: String!, $user: ID!) {
         addOrder(location: $location, prankee: $prankee, prank: $prank, dateTime: $dateTime, user: $user) {
         _id
         dateTime
@@ -39,6 +39,68 @@ export const ADD_ORDER = gql`
         user {
             _id
         }
+        }
+    }
+`;
+
+export const DELETE_ORDER = gql`
+    mutation Mutation($orderId: ID!) {
+        deleteOrder(orderId: $orderId) {
+        _id
+        dateTime
+        location
+        prank {
+            _id
+        }
+        prankee
+        user {
+            _id
+        }
+        }
+    }  
+`;
+
+export const DELETE_USER = gql`
+    mutation Mutation($userId: ID!) {
+        deleteUser(userId: $userId) {
+        email
+        _id
+        orders {
+            _id
+        }
+        password
+        username
+        }
+    }
+`;
+
+export const UPDATE_ORDER = gql`
+    mutation Mutation($orderId: ID!, $location: String, $prankee: String, $prank: ID, $dateTime: String, $user: ID) {
+        updateOrder(orderId: $orderId, location: $location, prankee: $prankee, prank: $prank, dateTime: $dateTime, user: $user) {
+        _id
+        dateTime
+        location
+        prank {
+            _id
+        }
+        prankee
+        user {
+            _id
+        }
+        }
+    }
+`;
+
+export const UPDATE_USER = gql`
+    mutation Mutation($userId: ID!, $email: String, $username: String, $password: String) {
+        updateUser(userId: $userId, email: $email, username: $username, password: $password) {
+        _id
+        orders {
+            _id
+        }
+        email
+        password
+        username
         }
     }
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,38 +1,87 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_USER = gql`
-    query viewUser {
+  query Query($userId: ID!) {
+    viewUser(userId: $userId) {
+      _id
+      orders {
         _id
-        email
-        orders {
-            _id
-        }
-        password
-        username
-        }
+      }
+      email
+      password
+      username
+    }
+  }
+`;
+
+export const QUERY_USERS = gql`
+  query Query($userId: ID) {
+    viewUsers(userId: $userId) {
+      _id
+      email
+      orders {
+        _id
+      }
+      password
+      username
+    }
+  }
+`;
+
+export const QUERY_PRANK = gql`
+  query Query($prankId: ID!) {
+    viewPrank(prankId: $prankId) {
+      _id
+      description
+      inPerson
+      price
+      title
+    }
+  }
 `;
 
 export const QUERY_PRANKS = gql`
-    query viewPrank {
-        _id
-        description
-        inPerson
-        price
-        title
+  query Query($prankId: ID) {
+    viewPranks(prankID: $prankId) {
+      _id
+      description
+      inPerson
+      price
+      title
     }
+  }
 `;
 
-export const QUERY_ORDERS = gql`
-    query viewOrder {
+export const QUERY_ORDER = gql`
+  query Query($orderId: ID!) {
+    viewOrder(orderId: $orderId) {
+      _id
+      dateTime
+      location
+      prank {
         _id
-        dateTime
-        location
-        prank {
-          _id
-        }
-        prankee
-        user {
-          _id
-        }
       }
+      prankee
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_USER_ORDERS = gql`
+  query Query($userId: ID!) {
+    viewUserOrders(userId: $userId) {
+      _id
+      dateTime
+      location
+      prank {
+        _id
+      }
+      prankee
+      user {
+        _id
+      }
+    }
+  }
 `;
