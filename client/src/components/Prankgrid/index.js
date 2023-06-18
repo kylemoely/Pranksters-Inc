f2f9ce4fb0e2bef9d2e836f76768807d99f93ef8
JackBurke7 <128829107+JackBurke7@users.noreply.github.com>
2023-06-18 14:28:19 -0400

    Updated prankgrid

--- client/src/components/Prankgrid/index.js
+++ client/src/components/Prankgrid/index.js
@@ -1,44 +1,54 @@
-import {React} from 'react';
+import React from 'react';
 import { QUERY_PRANKS } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
 import { Card, Row, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import pic from './images/prank.png'
-
+import pic from './images/prank.png';
 
 const Prankgrid = () => {
-    const { loading, data } = useQuery(QUERY_PRANKS);
-    const Pranks = data?.viewPranks || [];
-    console.log(data);
-    
-    
-    return (
-        <div>
-            {loading ? 
-            <div> Pranks have not yet been fetched! </div>
-            : <div style={{ margin:'100px 500px'}}>
-                {Pranks.map((Prank, i) => {
-                    return (
-                <Container fluid key={i}>
-                 <Row>
-                 <Card style={{ width: '30rem', margin: '20px', display:'block'}}>
-                 <Card.Img variant="top" src={pic} style={{maxHeight:'50vh', maxWidth:'70vh'}}/>
-                    <Card.Body style={{ textAlign: 'center'}}>
-                    <Card.Title style={{color: 'red', fontSize: '30px' }} >{Prank.title}</Card.Title>
-                    <Card.Text >{Prank.description}</Card.Text>
-                    <Card.Text style={{color: 'blue' }}>Price: ${Prank.price}.00</Card.Text>
-                    <Link style={{ border: 'solid black', borderRadius: '1rem', backgroundColor: 'lightBlue', padding: '2px' }} variant="primary" to={`/pranks/${Prank._id}`}>Purchase Prank Code: {Prank._id}</Link>
-                    </Card.Body>
-                    </Card>
-                    </Row>
-                    </Container>
-                    )
-                    })}
-               </div>
-        }
-        </div>
-    )
-}
+  const { loading, data } = useQuery(QUERY_PRANKS);
+  const Pranks = data?.viewPranks || [];
+  console.log(data);
 
+  return (
+    <Container style={{ textAlign: 'center' }}>
+      <Row style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+        {loading ? (
+          <div>Pranks have not yet been fetched!</div>
+        ) : (
+          Pranks.map((Prank, i) => (
+            <Card
+              key={i}
+              style={{ width: '30rem', margin: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+            >
+              <Card.Img
+                variant="top"
+                src={pic}
+                style={{ maxHeight: '50vh', maxWidth: '70vh', objectFit: 'contain' }}
+              />
+              <Card.Body style={{ textAlign: 'center' }}>
+                <Card.Title style={{ color: 'red', fontSize: '30px' }}>{Prank.title}</Card.Title>
+                <Card.Text>{Prank.description}</Card.Text>
+                <Card.Text style={{ color: 'blue' }}>Price: ${Prank.price}.00</Card.Text>
+                <Link
+                  style={{
+                    border: 'solid black',
+                    borderRadius: '1rem',
+                    backgroundColor: 'lightBlue',
+                    padding: '2px',
+                  }}
+                  variant="primary"
+                  to={`/pranks/${Prank._id}`}
+                >
+                  Purchase Prank Code: {Prank._id}
+                </Link>
+              </Card.Body>
+            </Card>
+          ))
+        )}
+      </Row>
+    </Container>
+  );
+};
 
 export default Prankgrid;
