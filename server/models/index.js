1e76519c47cb8f06a8a52a1729021674e678de13
jack burke <jeburkemusic@gmail.com>
2023-06-20 00:15:59 -0400

    removed //

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,9 @@
 const User = require('./user');
 const Order = require('./Order');
 const Prank = require('./Prank')
-// Import other models here if you have additional ones
 
 module.exports = {
   User,
   Order,
   Prank
-  // Add other models here
 };
